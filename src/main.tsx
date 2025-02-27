75a92afba15af653b50309bf0dcb002ae0b411e4
Naga <rafakenji23@gmail.com>
2025-02-27 12:02:24 -0700

    feat: implement internationalization

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import { IntroductionPage } from "./features/introduction/presentation/introduction-page";
 import "./main.scss";
 import { ExperiencePage } from "./features/experience/presentation/experience-page";
+import "./core/internationalization/i18n";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
