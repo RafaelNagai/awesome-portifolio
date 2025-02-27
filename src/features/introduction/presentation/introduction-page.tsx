eb0c8bf0a7e97c6da3ddbcbb5e19acb4c56f90a5
Naga <rafakenji23@gmail.com>
2025-02-26 21:05:44 -0700

    feat: adjust in introduction page

--- src/features/introduction/presentation/introduction-page.tsx
+++ src/features/introduction/presentation/introduction-page.tsx
@@ -1,19 +1,32 @@
 import React from "react";
 import "./sass/introduction.scss";
+import { IterativeBackground } from "../../iterative-background/presentation/iterative-background";
+import icon from "../../../assets/rafael-background.png";
 
 export const IntroductionPage: React.FC = () => {
   return (
-    <div className="introduction">
-      <div className="introduction__content">
-        <h1 className="introduction__title">Rafael Kenji Sales Nagai</h1>
-        <div className="introduction__summary">
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Commodi vel
-          quasi tempore laudantium, ipsa quisquam animi officiis aspernatur odio
-          impedit earum culpa, nobis suscipit similique eligendi. Nesciunt ab
-          iure doloribus.
+    <IterativeBackground src={icon}>
+      <div className="introduction">
+        <div className="introduction__content">
+          <h1 className="introduction__title">Rafael Kenji Sales Nagai</h1>
+          <div className="introduction__summary">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem,
+            sapiente fugit? Aspernatur laborum facere optio temporibus, quidem
+            quasi laboriosam exercitationem, aut libero laudantium debitis
+            distinctio hic suscipit ipsum, labore molestias! Unde, accusantium
+            eaque, id non pariatur rerum, sed amet labore repellat cumque natus
+            ratione. Numquam amet fugiat modi. Atque praesentium repudiandae
+            nulla ducimus autem alias debitis corporis optio laudantium amet.
+            Eaque, eum numquam aspernatur nesciunt, nemo aut officia suscipit
+            magni fugiat natus molestiae deserunt incidunt, aliquid voluptates
+            accusantium soluta minus a delectus provident beatae. Quas culpa
+            beatae maiores doloribus soluta! Fugit dolorum velit molestiae
+            inventore consequuntur et officiis consectetur officia possimus
+            culpa maiores rerum laborum modi, temporibus libero eos. Numquam
+            labore debitis dicta explicabo fugiat. Explicabo eum rem quis enim!
+          </div>
         </div>
       </div>
-      <div className="introduction__image" />
-    </div>
+    </IterativeBackground>
   );
 };
--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { IntroductionPage } from "./features/introduction/presentation/introduction-page";
-import { IterativeBackground } from "./features/iterative-background/presentation/iterative-background";
-import icon from "../src/assets/rafael.png";
 import "./main.scss";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <IterativeBackground src={icon}>
-      <IntroductionPage />
-    </IterativeBackground>
-    <div className="test"></div>
+    <IntroductionPage />
   </StrictMode>
 );
