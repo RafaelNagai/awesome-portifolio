a28396c1f1c4ce27f6a1e7196bf57270e000310a
Naga <rafakenji23@gmail.com>
2025-03-02 11:15:20 -0700

    feat: background color

--- src/features/contact/presentation/components/contact-button/contact-button.tsx
+++ src/features/contact/presentation/components/contact-button/contact-button.tsx
@@ -1,16 +1,21 @@
 import React from "react";
 import "./contact-button.scss";
+import { useTranslation } from "react-i18next";
 
 type ContactButtonProps = {
   isExpanded: boolean;
 };
 
 export const ContactButton: React.FC<ContactButtonProps> = ({ isExpanded }) => {
+  const { t } = useTranslation();
+
   return (
     <div
       className={`contact-button ${isExpanded && "contact-button--expanded"}`}
     >
-      <p>Buttton</p>
+      <p className={`${isExpanded && "contact-button--hide"}`}>
+        {t("contact")}
+      </p>
     </div>
   );
 };
