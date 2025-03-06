3b7ae13666d644d96dfbfde511c8e0ceb2a2ba3e
Naga <rafakenji23@gmail.com>
2025-03-06 01:56:21 -0700

    chore: clean and imrpove code

--- src/features/contact/presentation/contact-page.tsx
+++ src/features/contact/presentation/contact-page.tsx
@@ -9,26 +9,12 @@ export const ContactPage: React.FC = () => {
   const [isExpanded, setIsExpanded] = useState<boolean>(false);
 
   useEffect(() => {
-    const buttonElement =
-      document.querySelector<HTMLDivElement>(".contact-button");
     const onScrollHandler = () => {
-      if (ref.current && buttonElement) {
-        //expand
+      if (ref.current) {
         const heightScreen = window.innerHeight;
         const { top } = ref.current.getBoundingClientRect();
         const _isExpanded = top < heightScreen;
         setIsExpanded(_isExpanded);
-
-        //position
-        const relativeTopPage = ref.current.offsetTop;
-        const heightButton = buttonElement.getBoundingClientRect().height;
-        const paddingBottom = 16;
-        const topPosition =
-          window.scrollY + heightScreen - paddingBottom - heightButton;
-        let topElement = topPosition - relativeTopPage;
-        if (topElement <= 0 && !_isExpanded) {
-          buttonElement.style.top = `${topElement}px`;
-        }
       }
     };
     document.addEventListener("scroll", onScrollHandler);
