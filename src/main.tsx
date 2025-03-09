dd1d203775a6fcdd9b04bd0e1b5aac0aaf088409
Naga <rafakenji23@gmail.com>
2025-03-09 13:41:51 -0600

    feat: youtube section

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { IntroductionPage } from "./features/introduction/presentation/introduction-page";
 import "./main.scss";
-import { ExperiencePage } from "./features/experience/presentation/experience-page";
+import { ExperiencePage } from "./features/summarize/presentation/experience-page";
 import "./core/internationalization/i18n";
 import { ContactPage } from "./features/contact/presentation/contact-page";
 import { DescriptionPage } from "./features/description/presentation/description-page";
