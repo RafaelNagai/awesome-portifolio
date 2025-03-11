d633a9c78bb9fc8b72a99482c8609e0d6755b99c
Naga <rafakenji23@gmail.com>
2025-03-11 10:43:44 -0600

    feat: language section

---
+++ src/features/summarize/presentation/sections/language/language-section.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { useTranslation } from "react-i18next";
+import "./language-section.scss";
+import educationIcon from "../../../../../assets/icons/education-icon.png";
+
+export const LanguageSection: React.FC = () => {
+  const { t } = useTranslation();
+
+  return (
+    <div className="language-section">
+      <div className="language-section__title">
+        <h2>{t("experience.language.title")}</h2>
+        <img src={educationIcon} />
+      </div>
+      <p>{t("experience.language.description")}</p>
+
+      <div className="language-section__languages">
+        <p className="language-section__language">Portuguese</p>
+        <div className="language-section__divider"></div>
+        <p className="language-section__language">English</p>
+      </div>
+    </div>
+  );
+};
