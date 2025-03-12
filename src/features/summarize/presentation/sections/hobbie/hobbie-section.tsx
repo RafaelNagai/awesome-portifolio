dea94a7fc1c8191d2c822a6ac01e7f44c98b7d31
Naga <rafakenji23@gmail.com>
2025-03-11 19:36:32 -0600

    feat: hobbie selection

--- src/features/summarize/presentation/sections/hobbie/hobbie-section.tsx
+++ src/features/summarize/presentation/sections/hobbie/hobbie-section.tsx
@@ -2,6 +2,9 @@ import React from "react";
 import { useTranslation } from "react-i18next";
 import "./hobbie-section.scss";
 import icon from "../../../../../assets/icons/hobbie-icon.png";
+import skyrimImage from "../../../../../assets/hobbies/skyrim-icon.jpg";
+import lolImage from "../../../../../assets/hobbies/lol-icon.png";
+import valorantImage from "../../../../../assets/hobbies/valorant-icon.jpeg";
 
 export const HobbieSection: React.FC = () => {
   const { t } = useTranslation();
@@ -13,6 +16,24 @@ export const HobbieSection: React.FC = () => {
         <h2>{t("experience.hobbie.title")}</h2>
       </div>
       <p>{t("experience.hobbie.description")}</p>
+
+      <div className="hobbie-section__list">
+        <p className="hobbie-section__bookcover hobbie-section__hobbie--4">
+          Hobbies
+        </p>
+        <img
+          className="hobbie-section__hobbie hobbie-section__hobbie--3"
+          src={skyrimImage}
+        />
+        <img
+          className="hobbie-section__hobbie hobbie-section__hobbie--2"
+          src={lolImage}
+        />
+        <img
+          className="hobbie-section__hobbie hobbie-section__hobbie--1"
+          src={valorantImage}
+        />
+      </div>
     </div>
   );
 };
