2f806728f293fcde82d972e4fb4e49164d7221ec
Naga <rafakenji23@gmail.com>
2025-04-08 12:03:14 -0600

    chore: improve youtube embedded

--- src/features/summarize/presentation/sections/youtube/youtube-section.tsx
+++ src/features/summarize/presentation/sections/youtube/youtube-section.tsx
@@ -2,12 +2,13 @@ import React from "react";
 import { useTranslation } from "react-i18next";
 import "./youtube-section.scss";
 import youtubeIcon from "../../../../../assets/icons/youtube-icon.png";
+import LiteYouTubeEmbed from "react-lite-youtube-embed";
 
 export const YoutubeSection: React.FC = () => {
   const { t } = useTranslation();
 
   return (
-    <div className="youtube-section summarize-section">
+    <div className="youtube-section summarize-section" data-hover={true}>
       <div className="summarize-section__title summarize-section__title--right">
         <img
           className="summarize-section__image summarize__icon"
@@ -17,13 +18,16 @@ export const YoutubeSection: React.FC = () => {
       </div>
 
       <p>{t("experience.youtube.description")}</p>
-      <iframe
-        className="youtube-section__video"
-        src="https://www.youtube.com/embed/AaGEkTtCnP0"
-        title="YouTube video"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-        allowFullScreen
-      ></iframe>
+      <LiteYouTubeEmbed
+        id="AaGEkTtCnP0"
+        title="Welcome to Pub Dev Videos"
+        noCookie={true}
+        poster="mqdefault"
+        wrapperClass="youtube-section__video"
+        iframeClass="youtube-section__video--video"
+        activatedClass="youtube-section__video--activated"
+        playerClass="youtube-section__video--icon"
+      />
     </div>
   );
 };
